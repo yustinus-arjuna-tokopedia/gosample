2f1f2ddb830ee109428b390174b6b14d2a63b0f7
Qasim Zaidi <qasim@zaidi.me>
2017-01-19 15:57:39 +0700

    add gops and pre-commit hook

--- app.go
+++ app.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
+	"github.com/google/gops/agent"
 	"log"
 	"net/http"
 
-  "github.com/tokopedia/gosample/hello"
-	grace "gopkg.in/tokopedia/grace.v1"
-	logging "gopkg.in/tokopedia/logging.v1"
+	"github.com/tokopedia/gosample/hello"
+	"gopkg.in/tokopedia/grace.v1"
+	"gopkg.in/tokopedia/logging.v1"
 )
 
 func main() {
@@ -17,9 +18,14 @@ func main() {
 
 	debug := logging.Debug.Println
 
-  debug("app started") // message will not appear unless run with -debug switch
+	debug("app started") // message will not appear unless run with -debug switch
 
-  hwm := hello.NewHelloWorldModule()
+	// gops helps us get stack trace if something wrong/slow in production
+	if err := agent.Listen(nil); err != nil {
+		log.Fatal(err)
+	}
+
+	hwm := hello.NewHelloWorldModule()
 
 	http.HandleFunc("/hello", hwm.SayHelloWorld)
 	go logging.StatsLog()
